54eb6204a66683ddf5854fcee1a6fbd671dd0de6
Taha Ören <taha33oren@gmail.com>
2023-07-30 22:46:36 +0300

    Finish Choose Plan

--- planChoose/script.js
+++ planChoose/script.js
@@ -4,23 +4,80 @@ const midrightContainer = document.getElementsByClassName('midrightContainer');
 const rightImage = document.getElementById('rightImage');
 const leftImage = document.getElementById('leftImage');
 
+const leftText = document.getElementById('lightplanText');
+const rightText = document.getElementById('muscleplanText');
+
 let leftRemoved = false;
-// Function to remove the leftImage
+let rightRemoved = false;
+
+
 function removeLeftImage() {
     if(leftRemoved == false){
         leftImage.classList.add('hidden');
+        setTimeout(function() {
+            leftText.classList.add('visible');
         leftRemoved = true;
+        }, 400);
     }
 }
 
 
 rightImage.addEventListener('click', () => {
     if (leftRemoved == true) {
-        // Reinsert the leftImage back into the midleftContainer
         leftImage.classList.remove('hidden');
+        leftText.classList.remove('visible');
         leftRemoved = false;
     }
-    else{
+    else if (rightRemoved == false){
         removeLeftImage();
     }
 });
+
+rightText.addEventListener('click', () => {
+    if (leftRemoved == true) {
+        leftImage.classList.remove('hidden');
+        leftText.classList.remove('visible');
+        leftRemoved = false;
+    }
+    else if(rightRemoved == false){
+        removeLeftImage();
+    }
+});
+
+
+function removeRightImage() {
+    if(rightRemoved == false){
+        rightImage.classList.add('hidden');
+        setTimeout(function() {
+            rightText.classList.add('visible');
+        rightRemoved = true;
+        }, 400);
+    }
+}
+
+
+leftImage.addEventListener('click', () => {
+    if (rightRemoved == true) {
+
+        rightImage.classList.remove('hidden');
+        rightText.classList.remove('visible');
+        rightRemoved = false;
+    }
+    else if (leftRemoved == false){
+        removeRightImage();
+    }
+});
+
+leftText.addEventListener('click', () => {
+    if (rightRemoved == true) {
+
+        rightImage.classList.remove('hidden');
+        rightText.classList.remove('visible');
+        rightRemoved = false;
+    }
+    else if (leftRemoved == false){
+        removeRightImage();
+    }
+});
+
+
